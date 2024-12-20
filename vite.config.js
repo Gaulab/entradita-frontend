1a308249415d05bb096b29d01fb1cfc395ea4768
aguilarivan <ivanaguilarnardin@gmail.com>
2024-12-20 01:15:05 -0300

    event page2

--- vite.config.js
+++ vite.config.js
@@ -7,33 +7,35 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Poppins', 'sans-serif'],
-      },
-    },
-  },
   plugins: [react()],
+  
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  
   server: {
     host: '0.0.0.0',
-    port: 5173, // Puedes cambiar el puerto si lo deseas
+    port: 5173,
   },
+
   build: {
-    sourcemap: true, // Habilita los mapas de origen
+    sourcemap: true, 
     rollupOptions: {
-      external: ['@radix-ui/react-select'],
+      external: [],
+    },
+    commonjsOptions: {
+      include: [/node_modules/],
     },
   },
+
+  optimizeDeps: {
+    include: ['@radix-ui/react-select'],
+  },
+
   define: {
     'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
     'process.env.VITE_APP_NAME': JSON.stringify(process.env.VITE_APP_NAME),
   },
-  
 });
