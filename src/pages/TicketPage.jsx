c1ad8dc57af58d1fe44c10e0ed7bb06ac1ef72c0
aguilarivan <ivanaguilarnardin@gmail.com>
2025-01-13 21:59:55 -0300

    fix oG

--- src/pages/TicketPage.jsx
+++ src/pages/TicketPage.jsx
@@ -100,7 +100,7 @@ export default function TicketPage() {
 
   <meta property="og:title" content={`${data.event_name} - Ticket`} />
   <meta property="og:description" content={`Únete a nosotros en ${data.event_name} el ${data.event_date} en ${data.event_place}.`} />
-  <meta property="og:image" content={data.event_image_address || 'https://i.imgur.com/k4iUzTR.jpeg'} />
+  <meta property="og:image" content="https://i.imgur.com/k4iUzTR.jpeg" />
   <meta property="og:image:alt" content="Imagen representativa del evento" />
   <meta property="og:url" content={window.location.href} />
   <meta property="og:type" content="website" />
