c9bc8116f9825b86dac61bde11b6d79ee796fe3b
aguilarivan <ivanaguilarnardin@gmail.com>
2025-01-13 22:23:25 -0300

    try finx og

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import { HelmetProvider } from 'react-helmet-async';
 createRoot(document.getElementById('root')).render(
-    <App />
+    <HelmetProvider>
+        <App />
+    </HelmetProvider>
 )
