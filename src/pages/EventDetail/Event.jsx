cb5974e261f4551877892763874f2e582c1a4cf4
aguilarivan <ivanaguilarnardin@gmail.com>
2025-06-22 02:53:32 -0300

    refactor: replace dynamic progress value with a static value in EventDetail component

--- src/pages/EventDetail/Event.jsx
+++ src/pages/EventDetail/Event.jsx
@@ -80,8 +80,8 @@ export default function Event({ event }) {
       <CardContent>
         <div className="space-y-2">
           <div className="relative">
-            <Progress value={percentage} className="h-4 bg-gray-800  border border-gray-500" />
-            <div className="absolute top-0 left-0 h-full bg-gradient-to-r from-blue-500 to-purple-500 rounded-full" style={{ width: `${percentage}%` }} />
+            <Progress value={91} className="h-4 bg-gray-800  border border-gray-500" />
+            <div className="absolute top-0 left-0 h-full bg-gradient-to-r from-blue-500 to-purple-500 rounded-full" style={{ width: `${91}%` }} />
           </div>
           <div className="flex justify-between text-xs text-gray-400">
             <span>{percentage.toFixed(1)}%</span>
